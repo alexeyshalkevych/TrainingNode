806bf215b8507bc878afcec6e02ba4eaec7949ac
Alexey <valnistuy@gmail.com>
2020-07-09 10:05:56 +0300

    :refactor: changed project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const contactsApi = require("./controllers/API/contacts");
+
+require("dotenv").config();
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(morgan("combined"));
 
 app.use(contactsApi);
 
-const PORT = process.env.PORT || 4242;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (err) =>
   err
