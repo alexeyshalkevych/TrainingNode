ce504604f218039ed46a3ebb1365dc960cb90e21
Alexey <valnistuy@gmail.com>
2020-07-13 10:48:27 +0300

    :docs: create userLogin method and create POST/login request

--- src/users/userController.js
+++ src/users/userController.js
@@ -1,14 +1,15 @@
 const userModel = require("./userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userRegister = async (req, res, next) => {
   try {
     const { password, email } = req.body;
     const saltLengthToGenerate = 6;
 
-    const user = await userModel.findOne({ email });
+    const userByEmail = await userModel.findOne({ email });
 
-    if (user) {
+    if (userByEmail) {
       return res.status(409).send({ message: "Email in use" });
     }
 
@@ -29,6 +30,43 @@ const userRegister = async (req, res, next) => {
   }
 };
 
+const userLogin = async (req, res, next) => {
+  try {
+    const { password, email } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(401).send({ message: "Email or password is wrong" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).send({ message: "Email or password is wrong" });
+    }
+
+    const newToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    const userUpdate = await userModel.findByIdAndUpdate(user._id, {
+      token: newToken,
+    });
+
+    res
+      .status(201)
+      .json({
+        token: newToken,
+        user: {
+          email: userUpdate.email,
+          subscription: userUpdate.subscription,
+        },
+      });
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   userRegister,
+  userLogin,
 };
--- src/users/userRouter.js
+++ src/users/userRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const usersRouter = express.Router();
-const { userRegister } = require("./userController");
+const { userRegister, userLogin } = require("./userController");
 const { userRegisterValidation } = require("../middlewares/userMiddlewares");
 
-usersRouter.post("/users", userRegisterValidation, userRegister);
+usersRouter.post("/auth/register", userRegisterValidation, userRegister);
+usersRouter.post("/auth/login", userRegisterValidation, userLogin);
 
 module.exports = usersRouter;
