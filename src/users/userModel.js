883bf812b048385364cd86dbde570fb1e40d9c73
Alexey <valnistuy@gmail.com>
2020-07-13 08:57:34 +0300

    :docs: create users collection and create userModel

---
+++ src/users/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  subscription: {
+    type: String,
+    enum: ["free", "pro", "premium"],
+    default: "free",
+  },
+  token: { type: String, required: false },
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
